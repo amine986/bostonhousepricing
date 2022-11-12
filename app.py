3042471aa92b08cd9b66fd3ca8aa98db9823ddf0
Amine Zakaria <medaminezakaria@gmail.com>
2022-11-12 17:41:59 +0100

    This is my app

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
-regmodel=pickle.load(open('regmodel.pkl','rb'))
-scaler=pickle.load(open('scaling.pkl','rb'))
+regmodel=pickle.load(open("regmodel.pkl","rb"))
+scaler=pickle.load(open("scaling.pkl","rb"))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/predict_api',methods=['POST'])
+@app.route("/predict_api",methods=["POST"])
 
 def predict_api():
     data=request.json['data']
